f7a8bc9b4c51cbc67e110f5315cc96439605acd8
BRavinder <105974685+BRavinder@users.noreply.github.com>
2022-09-16 09:08:32 +0530

    Time: 53 ms (22.80%), Space: 43.2 MB (79.63%) - LeetHub

---
+++ 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countValidWords(String sentence) {
+        String regex = "^([a-z]+(-?[a-z]+)?)?(!|\\.|,)?$";
+        String r2 = "[^0-9]+";
+        String[] arr = sentence.split("\\s+");
+        int ans = 0;
+        for(String s: arr)
+        {
+            if(s.matches(regex) && s.matches(r2))
+            {
+                ans++;
+                //System.out.println(s);
+            }
+        }
+        return ans;
+    }
+}
+// Time complexity : O(n^2)
+// Space Complexity : O(n)
